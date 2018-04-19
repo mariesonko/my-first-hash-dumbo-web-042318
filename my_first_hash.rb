3c9e8aece4cad0358eb36bce81d1b6166e3b3128
Marie Sonko <mariesonko62@gmail.com>
2018-04-19 17:24:38 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -20,15 +20,11 @@ shipping_manifest ["oil paintings"]
 end
 
 def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
-                 }
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2,"oil paintings" => 3}
   # add 2 muskets to the shipping_manifest hash below
 shipping_manifest["muskets"] = 2
 shipping_manifest["gun powder"] = 4
   # add 4 gun powder to the shipping_manifest hash below
   # return the shipping_manifest hash below
-  shipping_manifest
 end
+shipping_manifest
