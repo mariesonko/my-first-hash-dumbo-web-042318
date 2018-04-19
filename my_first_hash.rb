1d67243977fc7f61db6e609e385d6a677899f8f8
Marie Sonko <mariesonko62@gmail.com>
2018-04-19 17:22:25 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,5 +30,5 @@ shipping_manifest["muskets"] = 2
 shipping_manifest["gun powder"] = 4
   # add 4 gun powder to the shipping_manifest hash below
   # return the shipping_manifest hash below
-  return shipping_manifest
+  shipping_manifest
 end
