7ba16099457dc2510669f0e1ccfeb80811c9496b
Marie Sonko <mariesonko62@gmail.com>
2018-04-19 17:14:54 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash(hash)
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 hash = {"swim" => "ocean", "laugh" => "happy", "dance" => "groove"}
-return hash 
+return hash
 end
 
 def shipping_manifest(the_manifest)
